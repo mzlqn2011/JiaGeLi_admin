dd6fe3ad2af7d7fa2de93e2b99dca0882d277f16
mzlqn_pro <593773036@qq.com>
2019-08-13 12:27:42 +0800

    Initial Commit

---
+++ JiaGeLi_admin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JiaGeLi_admin
+//
+//  Created by mac on 2019/8/13.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JiaGeLi_admin/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JiaGeLi_admin
+//
+//  Created by mac on 2019/8/13.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
